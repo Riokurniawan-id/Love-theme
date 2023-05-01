4db59c3416ae79352e1a25aea062022603662fd7
Riokurniawan-id <riosayang911@gmail.com>
2023-05-01 16:44:34 +0700

    second

--- script.js
+++ script.js
@@ -3,16 +3,12 @@ var canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Initialize the GL context
 var gl = canvas.getContext("webgl");
 if (!gl) {
   console.error("Unable to initialize WebGL.");
 }
 
-//Time
 var time = 0.0;
-
-//************** Shader sources **************
 
 var vertexSource = `
 attribute vec2 position;
@@ -170,8 +166,6 @@ void main(){
 }
 `;
 
-//************** Utility functions **************
-
 window.addEventListener("resize", onWindowResize, false);
 
 function onWindowResize() {
@@ -182,7 +176,6 @@ function onWindowResize() {
   gl.uniform1f(heightHandle, window.innerHeight);
 }
 
-//Compile shader and combine with source
 function compileShader(shaderSource, shaderType) {
   var shader = gl.createShader(shaderType);
   gl.shaderSource(shader, shaderSource);
@@ -193,8 +186,6 @@ function compileShader(shaderSource, shaderType) {
   return shader;
 }
 
-//From https://codepen.io/jlfwong/pen/GqmroZ
-//Utility to complain loudly if we fail to find the attribute/uniform
 function getAttribLocation(program, name) {
   var attributeLocation = gl.getAttribLocation(program, name);
   if (attributeLocation === -1) {
@@ -211,13 +202,9 @@ function getUniformLocation(program, name) {
   return attributeLocation;
 }
 
-//************** Create shaders **************
-
-//Create vertex and fragment shaders
 var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
 var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
 
-//Create shader programs
 var program = gl.createProgram();
 gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
@@ -225,37 +212,17 @@ gl.linkProgram(program);
 
 gl.useProgram(program);
 
-//Set up rectangle covering entire canvas
-var vertexData = new Float32Array([
-  -1.0,
-  1.0, // top left
-  -1.0,
-  -1.0, // bottom left
-  1.0,
-  1.0, // top right
-  1.0,
-  -1.0, // bottom right
-]);
+var vertexData = new Float32Array([-1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0]);
 
-//Create vertex buffer
 var vertexDataBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
 
-// Layout of our data in the vertex buffer
 var positionHandle = getAttribLocation(program, "position");
 
 gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(
-  positionHandle,
-  2, // position is a vec2 (2 values per component)
-  gl.FLOAT, // each component is a float
-  false, // don't normalize values
-  2 * 4, // two 4 byte float components per vertex (32 bit float is 4 bytes)
-  0 // how many bytes inside the buffer to start from
-);
+gl.vertexAttribPointer(positionHandle, 2, gl.FLOAT, false, 2 * 4, 0);
 
-//Set uniform handle
 var timeHandle = getUniformLocation(program, "time");
 var widthHandle = getUniformLocation(program, "width");
 var heightHandle = getUniformLocation(program, "height");
@@ -267,14 +234,11 @@ var lastFrame = Date.now();
 var thisFrame;
 
 function draw() {
-  //Update time
   thisFrame = Date.now();
   time += (thisFrame - lastFrame) / 1000;
   lastFrame = thisFrame;
 
-  //Send uniforms to program
   gl.uniform1f(timeHandle, time);
-  //Draw a triangle strip connecting vertices 0-4
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
   requestAnimationFrame(draw);
